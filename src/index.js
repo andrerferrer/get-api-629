e96319c0bc5290bc8bee88336a462b5e4ea8931a
andre ferrer <andrerferrer@hotmail.com>
2021-05-26 06:03:48 -0300

    finish lovecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('hello from index.js')
+
+// fetch the url
+// console log the response information
+
+
+// ALWAYS START WITH THIS
+// fetch(url)
+//   .then(response => response.json())
+//   .then((data) => {
+  //     console.log(data);
+  //   });
+
+const getAPI = (movieName) => {
+  const url = `http://www.omdbapi.com/?apikey=adf1f2d7&s=${movieName}`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      const movies = data.Search;
+      movies.forEach((movie) => {
+        const movieHTML = `
+          <li>
+            <p>${movie.Title}</p>
+            <img src="${movie.Poster}">
+          </li> 
+        `;
+        const ul = document.querySelector('#results');
+        ul.insertAdjacentHTML('beforeend', movieHTML);
+      })
+    });
+
+}
+
+const form = document.getElementById('search-movies');
+console.log(form);
+form.addEventListener('submit', (event) => {
+  // prevent default behavior of the event
+  event.preventDefault(); // stop the page from reloading
+  const input = document.getElementById('keyword');
+  console.log(input);
+  const movieName = input.value;
+  console.log(movieName);
+  
+  // before getting the api
+  // clear the ul
+  const ul = document.getElementById('results');
+  ul.innerHTML = '';
+
+  getAPI(movieName);
+});
